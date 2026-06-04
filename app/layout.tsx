e7ba903e0ac1e38b313379f5eff74fd2c1e29b44
erlandel <erlandelfino@gmail.com>
2026-06-04 15:23:31 -0400

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
   title: "Compra Electrodomésticos Muebles e Iluminación | Tienda Fadiar",
   description: "Descubre productos de calidad para tu hogar en la tienda oficial de Fadiar. Electrodomésticos, mobiliario e iluminación con envíos a toda Cuba. Compra online fácil y seguro.",
   verification: {
-    google: "google-site-verification=iziVXgg8234vKYvGu_cHoWAOX6K9nj96NecGPoNvONY",
+    google: "iziVXgg8234vKYvGu_cHoWAOX6K9nj96NecGPoNvONY",
   },
 };
 
